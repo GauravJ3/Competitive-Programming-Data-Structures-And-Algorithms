2a01038aad19a7369f0b48568447416b4a7f2ac9
Gaurav <gauravjoshi12379@gmail.com>
2019-07-19 13:11:00 +0530

    SPOJ Question

---
+++ Spoj/WILLITST.java
@@ -0,0 +1,22 @@
+/*
+prob link:- https://www.spoj.com/problems/WILLITST/
+idea :- idea here is to check if num is power of 2 or not
+Simple way to check the above is do check
+num&(num-1) == 0 or not
+*/
+import java.util.*;
+class WILLITST {
+	public static boolean isPowOf2(long a) {
+		if ((a&(a-1) == 0)) return true;
+		return false;
+ 	}
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		long n = kb.nextLong();
+		if (isPowOf2(n)) {
+			System.out.println("TAK");
+		}else {
+			System.out.println("NIE");
+		}
+	}
+}
